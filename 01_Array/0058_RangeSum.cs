d4337e0578596f5eaf089f20146be4210cb57626
Fly The Kite <1136542115@qq.com>
2026-09-09 20:08:48 +0800

    add comments to 58 区间和

--- 01_Array/0058_RangeSum.cs
+++ 01_Array/0058_RangeSum.cs
@@ -15,18 +15,22 @@ namespace TestNamespace
     {
         public static int Solution(int begin, int end, int[] nums)
         {
+            // 构建前缀和数组：array[i] = nums[0] + nums[1] + ... + nums[i]
             int[] array = new int[nums.Length];
             for (int i = 0; i < nums.Length; i++)
             {
                 if (i == 0)
                 {
-                    array[i] = nums[i];
+                    array[i] = nums[i];                    // 第一个元素直接赋值
                 }
                 else
                 {
-                    array[i] = nums[i] + array[i - 1];
+                    array[i] = nums[i] + array[i - 1];     // 当前元素 + 前面所有的和
                 }
             }
+            // 核心：区间 [begin, end] 的和 = 前缀和[end] - 前缀和[begin-1]
+            // 原理：array[end] 包含了 0~end 的所有元素
+            //       减去 array[begin-1] 就去掉了 0~begin-1 的部分，剩下 begin~end
             return array[end] - array[begin - 1];
         }
 
